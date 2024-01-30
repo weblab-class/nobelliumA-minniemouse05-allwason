9830c66bec37392b83548683a5acf3218b4cace2
minniemouse05 <66386557+minniemouse05@users.noreply.github.com>
2024-01-30 15:19:15 -0500

    entry area door styling

---
+++ client/src/components/pages/IntroPage.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "../../utilities.css";
+
+const IntroPage = (props) => {
+  return (
+    <>
+      <div> hello </div>
+    </>
+  );
+};
+
+export default IntroPage;
