2f243199589fa147f5abf758bc27d61d84f8fad1
Jasmine Shone <jasshone@gmail.com>
2024-01-31 01:44:43 -0500

    why

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -41,6 +41,8 @@ const App = () => {
         get("/api/friends", { userId: user._id }).then((response) => {
           console.log(response);
         });
+      } else {
+        console.log("fuck");
       }
     });
   }, []);
