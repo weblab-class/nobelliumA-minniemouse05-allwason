c38a744b8080acb09c23646be04924c739901f4d
minniemouse05 <66386557+minniemouse05@users.noreply.github.com>
2024-01-30 22:18:12 -0500

    more styling for info page

--- client/src/components/pages/Info.js
+++ client/src/components/pages/Info.js
@@ -19,14 +19,14 @@ const Info = (props) => {
               <div className="info-instruction-heading"> how to navigate</div>
               <div className="instruction-container">
                 <div className="info-instruction-text">
-                  -use the left and right key arrows to move your bear avatar around the room
+                  - use the left and right key arrows to move your bear avatar around the room
                 </div>
                 <div className="info-instruction-text">
-                  -when you walk near a certain feature, it’s button will pop up. click on the
+                  - when you walk near a certain feature, it’s button will pop up. click on the
                   button to enter that feature
                 </div>
                 <div className="info-instruction-text">
-                  -the navigation bar has various pages, like achievements, friending, and
+                  - the navigation bar has various pages, like achievements, friending, and
                   leaderboard for you to visit!
                 </div>
               </div>
@@ -35,11 +35,11 @@ const Info = (props) => {
               <div className="info-instruction-heading"> create your own story</div>
               <div className="instruction-container ">
                 <div className="info-instruction-text">
-                  -every achievement you earn will be used to add another chapter to your story
+                  - every achievement you earn will be used to add another chapter to your story
                   line, crafting your very own hero’s journey
                 </div>
                 <div className="info-instruction-text">
-                  -earn achievements through interacting on RoomCraft, whether it be by friending
+                  - earn achievements through interacting on RoomCraft, whether it be by friending
                   others, gaining exp from completing to-do list tasks, or more!
                 </div>
               </div>
