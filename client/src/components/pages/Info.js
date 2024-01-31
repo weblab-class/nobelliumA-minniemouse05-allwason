b26a80a12579f920dc9583ec025391654ba403a8
minniemouse05 <66386557+minniemouse05@users.noreply.github.com>
2024-01-31 00:35:15 -0500

    styling for info page

--- client/src/components/pages/Info.js
+++ client/src/components/pages/Info.js
@@ -15,34 +15,47 @@ const Info = (props) => {
             <div className="info-heading"> What is RoomCraft? </div>
             <div className="About-typewriter">become the most productive version of yourself! </div>
 
-            <div className="info-navigate-container">
-              <div className="info-instruction-heading"> how to navigate</div>
-              <div className="instruction-container">
-                <div className="info-instruction-text">
-                  - use the left and right key arrows to move your bear avatar around the room
-                </div>
-                <div className="info-instruction-text">
-                  - when you walk near a certain feature, it’s button will pop up. click on the
-                  button to enter that feature
-                </div>
-                <div className="info-instruction-text">
-                  - the navigation bar has various pages, like achievements, friending, and
-                  leaderboard for you to visit!
+            <div className="info-image-text-container">
+              <img
+                className="info-nav-image"
+                src="https://i.ibb.co/jgqMZf8/Screenshot-2024-01-31-at-12-24-36-AM.png"
+              />
+              <div className="info-navigate-container">
+                <div className="info-instruction-heading"> how to navigate</div>
+                <div className="instruction-container">
+                  <div className="info-instruction-text">
+                    - use the left and right key arrows to move your bear avatar around the room
+                  </div>
+                  <div className="info-instruction-text">
+                    - when you walk near a certain feature, it’s button will pop up. click on the
+                    button to enter that feature
+                  </div>
+                  <div className="info-instruction-text">
+                    - the navigation bar has various pages, like achievements, friending, and
+                    leaderboard for you to visit!
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="info-navigate-container">
-              <div className="info-instruction-heading"> create your own story</div>
-              <div className="instruction-container ">
-                <div className="info-instruction-text">
-                  - every achievement you earn will be used to add another chapter to your story
-                  line, crafting your very own hero’s journey
-                </div>
-                <div className="info-instruction-text">
-                  - earn achievements through interacting on RoomCraft, whether it be by friending
-                  others, gaining exp from completing to-do list tasks, or more!
+
+            <div className="info-image-text-container">
+              <div className="info-navigate-container">
+                <div className="info-instruction-heading"> create your own story</div>
+                <div className="instruction-container ">
+                  <div className="info-instruction-text">
+                    - every achievement you earn will be used to add another chapter to your story
+                    line, crafting your very own hero’s journey
+                  </div>
+                  <div className="info-instruction-text">
+                    - earn achievements through interacting on RoomCraft, whether it be by friending
+                    others, gaining exp from completing to-do list tasks, and more!
+                  </div>
                 </div>
               </div>
+              <img
+                className="info-story-image"
+                src="https://i.ibb.co/nbd9kSy/Screenshot-2024-01-31-at-12-02-04-AM.png"
+              />
             </div>
           </div>
         </>
